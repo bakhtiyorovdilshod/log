a4cbd23dcdf800fb068a828921b47344fbf101f5
Dilshod <dilshod_bakhtiyorov@mf.uz>
2023-09-06 14:22:17 +0500

    fixed merge

--- src/core/rabbit.py
+++ src/core/rabbit.py
@@ -1,39 +1,36 @@
-import asyncio
-import json
-
 from aio_pika import connect_robust, Message
-
-from src.core.elastic import ElasticConfig
 from dotenv import load_dotenv
-import os
+
+
+from src.utils.decorator import write_log
 
 load_dotenv()
 
 
 class RabbitMQ:
-    def __init__(self, queue_name, url):
-        self.queue_name = queue_name
+    def __init__(self,  url):
+        self.queue_name = None
         self.url = url
         self.connection = None
         self.channel = None
         self.queue = None
+        self.collection = None
+        self.index_elastic = None
 
     async def connect(self):
         self.connection = await connect_robust(self.url)
         self.channel = await self.connection.channel()
         self.queue = await self.channel.declare_queue(self.queue_name)
 
-    async def consume(self):
+    @write_log
+    async def consume(self, *args, **kwargs):
         async with self.queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    print(message.body)
-                    # x = ElasticConfig(host=os.environ.get('ELASTIC_HOST'), password='elastic', username='elastic')
-                    # await x.connect()
-                    # await x.save_data('test', body=json.loads(message.body))
+                    return message
 
     async def publish(self, message):
         await self.channel.default_exchange.publish(
-            Message(body=message.encode()),
+            Message(body=message),
             routing_key=self.queue_name
         )
---
+++ src/utils/elastic.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+
+from src.core.elastic import ElasticConfig
+from src.utils.logger import logger_config
+
+logger = logger_config(__name__)
+
+load_dotenv()
+
+
+async def write_data_elastic(index, body):
+    logger.info("Writing data to elastic")
+    elastic_config = ElasticConfig(
+        host=os.environ.get('ELASTIC_HOST'),
+        password=os.environ.get('ELASTIC_USERNAME'),
+        username=os.environ.get('ELASTIC_PASSWORD')
+    )
+    await elastic_config.connect()
+    resp = await elastic_config.save_data(index, body)
+
