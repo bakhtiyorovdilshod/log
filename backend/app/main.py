f4b79614457c4aced455658b4beeed56ae7ad2bf
Dilshod <dilshod_bakhtiyorov@mf.uz>
2023-09-08 15:27:43 +0500

    changing port

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,4 +2,4 @@ import uvicorn
 from backend.app.core.app import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=7070, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=7071, reload=True)
