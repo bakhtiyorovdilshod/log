16f7c8e693fd21b3e3050f313a993911b85299a3
yaxshilikovkomil <yaxshilikovkomil@gmail.com>
2023-09-11 11:07:58 +0500

    Indexation_templates done

--- backend/app/api/v1/rabbitmq_template.py
+++ backend/app/api/v1/rabbitmq_template.py
@@ -18,9 +18,15 @@ from .utils_crud import (
     retrieve_template,
     retrieve_rabbit_template,
     object_bson_name,
-    create_rabbit_consumer
+    create_Indexation_templates,
+    update_Indexation_templates,
+    delete_Indexation_templates
 )
 from bson import ObjectId, json_util
+from bson import ObjectId, json_util
+from fastapi.encoders import jsonable_encoder
+import json
+
 app = APIRouter()
 
 
@@ -33,7 +39,6 @@ async def create_templates(name: RabbitMQTemplateBase = Body(...)):
 
     names = str(name)
     response_object = await db.db['rabbit_template'].find_one({'name': names})
-    print(response_object, "vvvvv")
     if response_object:
         raise HTTPException(status_code=400, detail="already existing")
     json_query = jsonable_encoder(name)
@@ -70,6 +75,49 @@ async def delete_service_name(id: str):
     return {"status": 200, "message": "Service Name successfully deleted"}
 
 
-#---------------------------------------   Rabbit Consumer mongodb create  ----------------------------#
+# ---------------------------------------   Rabbit Consumer mongodb create  ----------------------------#
+
+
+@app.post("/create_Indexation_templates")
+async def create_Indexation_template(data: RabbitConsumerBase = Body(...)):
+    """create rabbit validation"""
+
+    queue_name = str(data.queue_name)
+    response_object = await db.db['Indexation_templates'].find_one({'queue_name': queue_name})
+    if response_object:
+        raise HTTPException(status_code=400, detail=f" {queue_name} already existing, please create another one!")
+    response = jsonable_encoder(data)
+    response_object = await create_Indexation_templates(response)
+    return ResponseModel(response_object, "success")
+
+
+@app.get("/get_all_Indexation_templates/")
+async def get_all_Indexation_templates():
+    """get alla rabbit validation"""
+
+    stores = []
+    response_objects = db.db['Indexation_templates'].find()
+    async for item in response_objects:
+        response = json.loads(json_util.dumps(item))
+        stores.append(response)
+    return stores
+
+
+@app.put("/update_Indexation_templates{id}")
+async def update_Indexation_templates(id: str, data: RabbitConsumerBase = Body(...)):
+    # id = ObjectId(id)
+    request = {k: v for k, v in data.model_dump().items() if v is not None}
+    updated_validation = await update_Indexation_templates(id, data)
+    update_data = data.model_dump()
+
+
+@app.delete("/delete_Indexation_templates{id}")
+async def delete_Indexation_templates(id: str):
+    id = ObjectId(id)
+    response_object = await db.db['Indexation_templates'].delete_one({"_id": id})
+    if response_object.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"status": 200, "message": "Service Name successfully deleted"}
+
 
 
--- backend/app/api/v1/utils_crud.py
+++ backend/app/api/v1/utils_crud.py
@@ -63,36 +63,52 @@ async def object_bson_name(name: str):
 # --------------------------------------------  RabbitMQ Consumer template  -------------------------#
 
 
-def rabbit_consumer_helper(data) -> dict:
+def rabbit_validation_helper(data) -> dict:
     return {
-          "id": str(data['_id']),
-          "queue_name": data['queue_name'],
-          "table_name": data['table_name'],
-          "validation": [
-            {
-              "method": data["method"],
-              "fields": [
-                {
-                  "key": data["key"],
-                  "type": data['type']
-                }
-              ]
-            }
-          ]
-        }
+        "id": str(data['_id']),
+        "queue_name": data['queue_name'],
+        "table_name": data['table_name']}
+        #   "validation": [
+        #     {
+        #       "method": data["method"],
+        #       "fields": [
+        #         {
+        #           "key": data["key"],
+        #           "type": data['type']
+        #         }
+        #       ]
+        #     }
+        #   ]
+        # }
 
 
-async def retrieve_rabbit_consumer():
-    """for rabbit consumer data with validation"""
+async def retrieve_template_validation_rabbit():
+    """for rabbit validation data with validation"""
     rabbit_list = []
-    async for item in db.db['rabbit_consumer'].find():
-        rabbit_list.append(rabbit_consumer_helper(item))
+    async for item in db.db['Indexation_templates'].find():
+        rabbit_list.append(rabbit_validation_helper(item))
     return rabbit_list
 
 
-async def create_rabbit_consumer(data: dict) -> dict:
-    """RabbitMQ Consumer to mongodb create"""
+async def create_Indexation_templates(data: dict) -> dict:
+    """RabbitMQ validation to mongodb create"""
 
-    service_query = await db.db['rabbit_consumer'].insert_one(data)
-    template_query = await db.db['rabbit_consumer'].find_one({"_id": service_query.inserted_id})
-    return rabbit_consumer_helper(template_query)
+    service_query = await db.db['Indexation_templates'].insert_one(data)
+    template_query = await db.db['Indexation_templates'].find_one({"_id": service_query.inserted_id})
+    return rabbit_validation_helper(template_query)
+
+
+async def update_Indexation_templates(id: str, data: dict):
+    if len(data) < 1:
+        return False
+    service_template = await db.db['Indexation_templates'].update_one({"_id": ObjectId(id)}, {"$set": data})
+    if service_template:
+        return True
+    return False
+
+
+async def delete_Indexation_templates(id: str):
+    service_template = await db.db['Indexation_templates'].find_one({"_id": ObjectId(id)})
+    if service_template:
+        await service_template.delete_one({"_id": ObjectId(id)})
+        return True
